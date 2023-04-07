67dfc4da0bec248ef691ba09bfb7e2cd4b61c94b
unit551 <129839067+unit551@users.noreply.github.com>
2023-04-07 13:01:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -10,12 +10,76 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
 
+var swiperPopular = new Swiper(".popular__container", {
+	spaceBetween: 32,
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	loop: true,
+	
+	navigation: {
+		nextEl: ".swipper-button-next",
+		prevEl: ".swipper-button-prev",
+	},
+});
+
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+// 1. Select each item
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    // 2. Select each header click
+    accordionHeader.addEventListener('click', () =>{
+        // 7. Create the tag
+        const openItem = document.querySelector('.accordion-open')
+        
+        // 5. Call toggle item function
+        toggleItem(item)
+
+        // 8. Check if the class exists
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+// 3. Create a constant type function
+const toggleItem = (item) =>{
+    // 3.1 Create the tag
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    // 6. If there is another element that contains the class accordion-open remove its class
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        // 4. Add the maximum height of the content
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight, sectionTop = current.offsetTop - 58, sectionId = current.getAttribute('id'), sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
